ea84e3f15fcca76b8195f273432b35628167d61e
thiago <dbc.melo@unimedpoa.com.br>
2022-04-17 17:20:41 -0300

    Swager Inquilino

--- sis.apartamentos.com.br/src/main/java/sis/apartamentos/com/br/model/Inquilino.java
+++ sis.apartamentos.com.br/src/main/java/sis/apartamentos/com/br/model/Inquilino.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import io.swagger.annotations.ApiModelProperty;
 import sis.apartamentos.com.br.utils.Constantes;
 
 @Entity
@@ -25,27 +26,35 @@ public class Inquilino implements Serializable {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "inquilino_seq")
 	@SequenceGenerator(name = "inquilino_seq", sequenceName = "inquilino_seq", initialValue = 1, allocationSize = 1)
 	@Column(name = "ID", nullable = false, unique = true)
+	@ApiModelProperty(value = "ID do inquilino", example = "1", required = true)
 	private Long id;
 
 	@Column(name = "NOME", length = 90, nullable = false)
+	@ApiModelProperty(value = "Nome do inquilino", example = "Thiago", required = true)
 	private String nome;
 
 	@Column(name = "NOME_ABREVIADO", length = 20, nullable = false)
+	@ApiModelProperty(value = "Nome Abreviado do inquilino", example = "T. Henrique", required = true)
 	private String nomeAbreviado;
 
 	@Column(name = "EMAIL", length = 90)
+	@ApiModelProperty(value = "Email do inquilino", example = "thiago.henrique.25@hotmail.com")
 	private String email;
 
 	@Column(name = "CONTATO", length = 15)
+	@ApiModelProperty(value = "Contato do inquilino", example = "92991679486")
 	private String contato;
 
 	@Column(name = "STATUS", length = 20, nullable = false)
+	@ApiModelProperty(value = "Status do inquilino", example = "ATIVO", required = true)
 	private String status = Constantes.ATIVO;
 
 	@Column(name = "GENERO", length = 20, nullable = false)
+	@ApiModelProperty(value = "Genero do inquilino", example = "MASCULINO", required = true)
 	private String genero = Constantes.MASCULINO;
 
 	@Column(name = "cpf", length = 11, unique = true)
+	@ApiModelProperty(value = "Cpf do inquilino", example = "88371468252")
 	private String cpf;
 
 	public Inquilino() {
