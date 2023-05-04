4be1d5a9b3a5509e1221a46eb8160464bf1e6cbd
thiago <dbc.melo@unimedpoa.com.br>
2023-05-03 21:39:49 -0300

    Add filtro com paginação no diario.

--- sis.apartamentos.com.br/src/main/java/sis/apartamentos/com/br/api/v1/controller/DiarioController.java
+++ sis.apartamentos.com.br/src/main/java/sis/apartamentos/com/br/api/v1/controller/DiarioController.java
@@ -1,15 +1,16 @@
 package sis.apartamentos.com.br.api.v1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import sis.apartamentos.com.br.api.v1.dto.diario.DiarioFilterDTO;
 import sis.apartamentos.com.br.api.v1.dto.diario.DiarioPostDTO;
 import sis.apartamentos.com.br.api.v1.dto.diario.DiarioResponseDTO;
 import sis.apartamentos.com.br.api.v1.icontroller.DiarioControllerOpenApi;
 import sis.apartamentos.com.br.api.v1.mapper.DiarioMapper;
+import sis.apartamentos.com.br.domain.model.Diario;
 import sis.apartamentos.com.br.domain.repository.DiarioRepository;
 import sis.apartamentos.com.br.infra.exception.EntidadeNaoEncontradaException;
 import sis.apartamentos.com.br.infra.exception.EntidadeRestricaoDeDadosException;
@@ -33,6 +34,12 @@ public class DiarioController implements Serializable, DiarioControllerOpenApi {
 
     @Autowired
     private DiarioMapper diarioMapper;
+
+    @GetMapping
+    @Override
+    public Page<Diario> pesquisar(DiarioFilterDTO diarioFilterDTO, Pageable pageable) {
+        return diarioRepository.filtrar(diarioFilterDTO, pageable);
+    }
 
     @PostMapping
     @Override
--- sis.apartamentos.com.br/src/main/java/sis/apartamentos/com/br/api/v1/icontroller/DiarioControllerOpenApi.java
+++ sis.apartamentos.com.br/src/main/java/sis/apartamentos/com/br/api/v1/icontroller/DiarioControllerOpenApi.java
@@ -1,13 +1,20 @@
 package sis.apartamentos.com.br.api.v1.icontroller;
 
 import io.swagger.annotations.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import sis.apartamentos.com.br.api.v1.dto.diario.DiarioFilterDTO;
 import sis.apartamentos.com.br.api.v1.dto.diario.DiarioPostDTO;
 import sis.apartamentos.com.br.api.v1.dto.diario.DiarioResponseDTO;
+import sis.apartamentos.com.br.domain.model.Diario;
 
 import javax.servlet.http.HttpServletResponse;
 
 @Api(tags = "Diario")
 public interface DiarioControllerOpenApi {
+
+    @ApiOperation("Busca todos diarios paginados")
+    Page<Diario> pesquisar(DiarioFilterDTO diarioFilterDTO, Pageable pageable);
 
     @ApiOperation("Cria um novo diario")
     @ApiResponses({
